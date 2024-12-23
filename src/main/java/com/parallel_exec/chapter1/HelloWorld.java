75005a7191c09f35e4a9a4e8df03d208ef94458a
ZQCHANG <changzhiqian374@gmail.com>
2024-12-23 13:50:47 +0900

    编写hello world程序, 主要用于测试git分支

---
+++ src/main/java/com/parallel_exec/chapter1/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.parallel_exec.chapter1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        // 启动第一个线程 t1
+        new Thread(() -> {
+            System.out.println("hello world -- t1");
+        }, "t1").start();
+
+        // 启动第二个线程 t2
+        new Thread(() -> {
+            System.out.println("hello world -- t2");
+        }, "t2").start();
+
+    }
+}
